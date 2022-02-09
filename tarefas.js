7c7758211e223463c21c312febcfb76fd72c774b
Douglas Miranda <74260286+doug535@users.noreply.github.com>
2022-02-09 12:24:18 -0300

    terminado a lista de tarefas

--- tarefas.js
+++ tarefas.js
@@ -1,8 +1,18 @@
-function add() {
-    const ul = document.querySelector("ul")
-    const input = document.querySelector("input")
-    const tarefa = input.value
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
+const tarefas = []
 
-    const li = document.createElement("li")
-    li.innerText = tarefa
+function add(){
+    if(input.value != "")
+    tarefas.push(input.value)
+    input.value = ""
+    render()
+}
+function render(){
+    ul.innerHTML = null
+    tarefas.forEach(function(tarefa) {
+        const li = document.createElement("li")
+        li.innerText = tarefa
+        ul.appendChild(li)
+    })
 }
